e7f3710384a9f37138d070c6b8c64de3aaba64c0
Oscar Goh <oscar.goh@gmail.com>
2017-02-20 16:09:16 -0600

    initial commit

---
+++ ch1/hello-world-3.py
@@ -0,0 +1,7 @@
+from sklearn import tree
+features = [[140, 0], [130, 0], [150, 1], [170, 1], [120, 0], [110, 0], [160, 1], [180, 1]] # [weight, type] 0 is bumpy
+labels = [0, 0, 1, 1, 0, 0, 1, 1] # 0 is apple, 1 is orange
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print clf.predict([[130, 1], [160, 1], [160, 0], [170, 0]])
+
